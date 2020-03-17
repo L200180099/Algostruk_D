1960c3739101f1f6bec49621ce3ca26bb5fbeb88
Nadya Ayu Widya <48179439+L200180099@users.noreply.github.com>
2020-03-17 22:33:55 +0700

    Update Latihan_Modul_4.py

--- Latihan_Modul_4.py
+++ Latihan_Modul_4.py
@@ -36,8 +36,14 @@ c7 = MhsTIF('Galuh', 5, 'Wonogiri', 245000)
 c8 = MhsTIF('Janto', 23, 'Klaten', 245000)
 c9 = MhsTIF('Hasan', 64, 'Karanganyar', 270000)
 c10 = MhsTIF('Khalid', 29, 'Purwodadi', 265000)
-
 Daftar = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10]
+
+#Mencetak nama mahasiswa yg beralamat Klaten
+target = 'Klaten'
+for i in Daftar:
+    if i.kotaTinggal == target:
+        print(i.nama + ' tinggal di '+ target)
+        
 #Mencari data terkecil
 def min(Daftar):
     nama = []
